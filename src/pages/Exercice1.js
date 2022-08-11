ca625b285019835123ece831e22bc401262a506f
ollivie13u <56250800+ollivie13u@users.noreply.github.com>
2022-08-11 17:38:02 +0200

    photo_mains

--- src/pages/Exercice1.js
+++ src/pages/Exercice1.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Exercice1.css';
 import Header from './Components/Header';
+import Ing_Mains from '../assets/ing_mains.webp';
 
 function Exercice1() {
   return (
   <div>
     <div>
       <Header />
+    </div>
+    <div>
+        <img className='ing_mains' src={Ing_Mains} alt="Logo" />
     </div>
   </div>
   );
